2baa72f23604b364dcefd0fc7b4a4d08c3d2ad5c
unknown <victoriasugden@yahoo.co.uk>
2024-03-01 15:44:47 +0000

    added css page and bootstrap

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,8 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import * as bootstrap from 'bootstrap'
-
+import './index.css'
+import bootstrap from 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
