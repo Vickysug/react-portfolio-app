14122bf4a5d371ad39775b7a98f3566488040d97
unknown <victoriasugden@yahoo.co.uk>
2024-03-01 16:21:02 +0000

    added image

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,17 @@ function App() {
   return ( <div>
     <h1>Victoria Sugden</h1>
     <h2>Software Engineer</h2>
+    <div><br></br><div id=" photo" className="row row-cols-1 row-cols-md-2 g-4">
+  <div className="col">
+    <div className="card">
+      <img src=".images/me.jpg" className="card-img-top" alt="..."/>
+      <div className="card-body">
+        <h5 className="card-title">Card title</h5>
+        <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+      </div>
+    </div>
+  </div></div></div>
+    
 
     <div className="row row-cols-1 row-cols-md-2 g-4">
   <div className="col">
