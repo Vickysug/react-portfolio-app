553d29ab4c0cd713ce46970e6c14b815a3ab2529
unknown <victoriasugden@yahoo.co.uk>
2024-03-01 16:09:26 +0000

    added bootstrap cards

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import App from './App'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import * as bootstrap from 'bootstrap'
 import './index.css'
-import bootstrap from 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css'
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
