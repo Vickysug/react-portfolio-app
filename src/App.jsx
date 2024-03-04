b857a324d4ea54a58a338aced387ffc9df75752b
unknown <victoriasugden@yahoo.co.uk>
2024-03-04 12:51:49 +0000

    added picture to header

--- src/App.jsx
+++ src/App.jsx
@@ -5,15 +5,16 @@ function App() {
   return ( //1st row and column for name & title. 2nd column picture and a litle information
   <div>
     <div><div id="top" className="row row-cols-1 row-cols-md-2 g-4">
-  <div className="col"></div>
+    <div className="col">    
     <h1>Victoria Sugden</h1>
     <h3>Software Engineer</h3>
+  </div>
     
   <div className="col">
     <div className="card">
-      <img src="" className="card-img-top" alt="..."/>
+    <img id="me-pic" src="src/images/me.jpg" className="card-img-top" alt="image of me"/>
       <div className="card-body">
-        <h5 className="card-title"></h5>
+        <h3 className="card-title"></h3>
         <p className="card-text">a little about me and my skills here plus a cv link. See some of my projects below.</p>
       </div>
     </div>
@@ -25,7 +26,7 @@ function App() {
     <div className="card">
       <img src="..." className="card-img-top" alt="..."/>
       <div className="card-body">
-        <h5 className="card-title">Project 1</h5>
+        <h3 className="card-title">Project 1</h3>
         <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
       </div>
     </div>
@@ -34,7 +35,7 @@ function App() {
     <div className="card">
       <img src="..." className="card-img-top" alt="..."/>
       <div className="card-body">
-        <h5 className="card-title">Project 2</h5>
+        <h3 className="card-title">Project 2</h3>
         <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
       </div>
     </div>
@@ -43,7 +44,7 @@ function App() {
     <div className="card">
       <img src="..." className="card-img-top" alt="..."/>
       <div className="card-body">
-        <h5 className="card-title">Project 3</h5>
+        <h3 className="card-title">Project 3</h3>
         <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
       </div>
     </div>
@@ -52,13 +53,14 @@ function App() {
     <div className="card">
       <img src="..." className="card-img-top" alt="..."/>
       <div className="card-body">
-        <h5 className="card-title">Project 4</h5>
+        <h3 className="card-title">Project 4</h3>
         <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
       </div>
     </div>
   </div>
 </div>
   </div>
+
 
 
 
