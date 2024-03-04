7d53710104989440c0e653f9482c4e2cccf2b4f0
unknown <victoriasugden@yahoo.co.uk>
2024-03-04 13:30:28 +0000

    changed header card to a column

--- src/App.jsx
+++ src/App.jsx
@@ -5,21 +5,21 @@ function App() {
   return ( //1st row and column for name & title. 2nd column picture and a litle information
   <div>
     <div><div id="top" className="row row-cols-1 row-cols-md-2 g-4">
-    <div className="col">    
+    <div className="col-4">    
     <h1>Victoria Sugden</h1>
     <h3>Software Engineer</h3>
+    <button>View CV</button> <button>About Me</button> <button>Contact</button>
+    
   </div>
-    
-  <div className="col">
-    <div className="card">
-    <img id="me-pic" src="src/images/me.jpg" className="card-img-top" alt="image of me"/>
-      <div className="card-body">
-        <h3 className="card-title"></h3>
-        <p className="card-text">a little about me and my skills here plus a cv link. See some of my projects below.</p>
-      </div>
-    </div>
-  </div></div></div>
-    
+  <div class="row">
+  <div class="col-8">col-8</div>
+  <img id="me-pic" src="src/images/me.jpg" className="card-img-top" alt="image of me"/>
+    <div><p className="card-text">a little about me and my skills here plus a cv link. See some of my projects below.</p></div>
+</div></div></div>
+<div class="row">
+  <div class="col-8"></div>
+  <div class="col-4"></div>
+</div>
 
     <div className="row row-cols-1 row-cols-md-2 g-4">
   <div className="col">
